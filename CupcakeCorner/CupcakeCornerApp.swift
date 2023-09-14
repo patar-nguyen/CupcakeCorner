86a84f588e4a17284dce88d706608fca8f7bb9ee
Patrick Nguyen <patar.nguyen@gmail.com>
2023-09-14 10:27:09 -0400

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Patrick Nguyen on 2023-09-14.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
