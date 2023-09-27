2dbb1fb1ebf4d1e5e19b6673711448186413faf7
Patrick Nguyen <patar.nguyen@gmail.com>
2023-09-27 10:47:03 -0400

    initial commit

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Patrick Nguyen on 2023-09-14.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddressView()
+    }
+}
---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Patrick Nguyen on 2023-09-14.
+//
+
+import Foundation
